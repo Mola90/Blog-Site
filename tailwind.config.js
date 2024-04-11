c871b1d06c323cc60926c76d2f5a26b4661c8f35
Mola Dafa <mola_d@hotmail.com>
2024-04-11 16:38:16 +1000

    npm packages and server.js set up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/view/**/*.handlebars', // Adjust the path as per your project structure
+  './public/**/*.html',
+  './src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
