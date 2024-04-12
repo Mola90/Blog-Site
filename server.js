e4cc48cd7b7680afca78243597b773bb999ddf21
Mola Dafa <mola_d@hotmail.com>
2024-04-12 17:26:39 +1000

    css working

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 
 app.engine('handlebars', hbs.engine);
+
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/view/**/*.handlebars', // Adjust the path as per your project structure
+  content: ['./views/**/*.handlebars', // Adjust the path as per your project structure
   './public/**/*.html',
   './src/**/*.js'],
   theme: {
